ce85a469e46dc411f0b80eeab7e90439c1f05efe
Bernardo Bechtold <bernardo.bechtold@shopify.com>
2023-08-04 14:40:22 -0300

    🐛 fix the error treatment for register route

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,7 +1,8 @@
+import express from 'express'
 import authService from '../services/auth.service'
 import createError from 'http-errors'
 
-async function register(req, res, next) {
+async function register(req: express.Request, res: express.Response) {
   try {
     const user = await authService.register(req.body)
     res.status(200).json({
@@ -10,7 +11,7 @@ async function register(req, res, next) {
       data: user,
     })
   } catch (error) {
-    next(createError(error.statusCode, error.message))
+    res.status(error.statusCode).json({ error: error.message })
   }
 }
 
--- backend/src/services/auth.service.ts
+++ backend/src/services/auth.service.ts
@@ -8,19 +8,35 @@ import createError from 'http-errors'
 const prisma = new PrismaClient()
 dotenv.config()
 
+async function createUser(data) {
+  const { email, name, password } = data
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 8)
+    const user = await prisma.user.create({
+      data: {
+        email,
+        name,
+        password: hashedPassword,
+      },
+    })
+    return user
+  } catch (error) {
+    console.error(error.message)
+    console.error(error.code)
+    throw createError.InternalServerError('Error creating user')
+  }
+}
+
 async function register(data) {
   const { email, name, password } = data
+
   if (!email || !name || !password) {
     throw createError.Unauthorized('Email or password not valid')
   }
-  const hashedPassword = await bcrypt.hash(password, 8)
-  const user = await prisma.user.create({
-    data: {
-      email,
-      name,
-      password: hashedPassword,
-    },
-  })
+
+  const user = await createUser(data)
+
   data.accessToken = await signAccessToken(user)
   return data
 }
